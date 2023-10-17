06ff81143a670ee391a7bcc94cc33725dd297677
Jacob Smith <jacobsmithsinbox@gmail.com>
2023-10-17 01:46:47 -0600

    Basic route implementation

--- include/web/route.h
+++ include/web/route.h
@@ -2,3 +2,59 @@
  *  Defines a route for resources on a webpage 
  * 
  */
+
+// Header guard
+#pragma once
+
+// Standard library
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include <web/web.h>
+
+struct web_route_s
+{
+    char name[512];
+    char path[1024];
+};
+
+
+/** !
+ * Allocate memory for a server route
+ * 
+ * @param pp_web_route return
+ * 
+ * @return 1 on success, 0 on error
+ */
+int web_route_create ( web_route **const pp_web_route );
+
+/** !
+ * Construct a server route from a file
+ * 
+ * @param pp_web_route return
+ * @param p_path       path to file
+ * 
+ * @return 1 on success, 0 on error 
+ */
+int web_route_construct ( web_route **const pp_web_route, const char *const p_path );
+
+/** !
+ * Construct a server route from a json value
+ * 
+ * @param pp_web_route return
+ * @param p_value      the json value
+ * 
+ * @return 1 on success, 0 on error
+ */
+int web_route_from_json ( web_route **const pp_web_route, const json_value *const p_value );
+
+/** !
+ * Destroy and deallocate a server route and all it's contents.
+ * 
+ * @param pp_web_route the web route, return
+ * 
+ * @return 1 on success, 0 on error 
+ */
+int web_route_destroy ( web_route **const pp_web_route );
---
+++ route.c
@@ -0,0 +1,119 @@
+#include <web/route.h>
+
+int web_route_create ( web_route **const pp_web_route )
+{
+
+    // Argument check
+    if ( pp_web_route == (void *) 0 ) goto no_web_route;
+
+    // Initialized data
+    web_route *p_web_route = WEB_REALLOC(0, sizeof(web_route));
+
+    // Error check
+    if ( p_web_route == (void *) 0 ) goto no_mem;
+
+    // Zero set the struct
+    memset(p_web_route, 0, sizeof(web_route));
+
+    // Return a pointer to the caller
+    *pp_web_route = p_web_route;
+
+    // Success
+    return 1;
+
+    // Error handling
+    {
+
+        // Argument errors
+        {
+            no_web_route:
+                #ifndef NDEBUG
+                    printf("[web] Null pointer provided for parameter \"pp_web_server\" in call to function \"%s\"\n", __FUNCTION__);
+                #endif
+
+                // Error
+                return 0;
+        }
+
+        // Standard library errors
+        {
+            no_mem:
+                #ifndef NDEBUG
+                    printf("[Standard Library] Failed to allocate memory in call to function \"%s\"\n", __FUNCTION__);
+                #endif
+
+                // Error
+                return 0;
+        }
+    }
+}
+
+int web_route_construct ( web_route **const pp_web_route, const char *const p_path );
+
+int web_route_from_json ( web_route **const pp_web_route, const json_value *const p_value )
+{
+
+    // Argument check
+    if ( pp_web_route == (void *) 0 ) goto no_web_route;
+    if ( p_value      == (void *) 0 ) goto no_value;
+
+    // Initialized data
+    web_route  *p_web_route = 0;
+
+    // Allocate a web route
+    if ( web_route_create(&p_web_route) == (void *) 0 );
+
+    // Parse the json object
+    if ( p_value->type == JSON_VALUE_STRING )
+        strcpy(p_web_route->path, p_value->string);
+
+    // Return a pointer to the caller
+    *pp_web_route = p_web_route;
+
+    // Success
+    return 1;
+
+    // Error handling
+    {
+        
+        // Argument errors
+        {
+            no_web_route:
+                #ifndef NDEBUG
+                    printf("[web] Null pointer provided for parameter \"pp_web_server\" in call to function \"%s\"\n", __FUNCTION__);
+                #endif
+
+                // Error
+                return 0;
+
+            no_value:
+                #ifndef NDEBUG
+                    printf("[web] Null pointer provided for parameter \"p_value\" in call to function \"%s\"\n", __FUNCTION__);
+                #endif
+
+                // Error
+                return 0;
+                
+        }
+
+        // Standard library errors
+        {
+            no_mem:
+                #ifndef NDEBUG
+                    printf("[Standard Library] Failed to allocate memory in call to function \"%s\"\n", __FUNCTION__);
+                #endif
+
+                // Error
+                return 0;
+        }
+    }
+}
+
+int web_route_destroy ( web_route **const pp_web_route )
+{
+
+    // TODO: Implement
+
+    // Error
+    return 0;
+}
